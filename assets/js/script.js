dbd6ca27e7488b1394f73ce108a30133646b2f0c
Kyle <kj.mckinley@gmail.com>
2021-03-13 02:17:15 -0600

    start of js, moving to feature branch

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// GEEK-BIZ QUIZ
+
+// This .js file is meant to handle and generate the questions in the form of array objects
+
+// Array of objects containing questions
+var questionAray = [
+    {
+        header: "Everything is TRUE in The Following Statement EXCEPT:",
+        question: "",
+        options: [],
+        answer: ""
+    },
+    {
+        header: "TRUE or FALSE:",
+        question: "",
+        options: [],
+        answer: ""
+    },
+    {
+        header: "Which Word in the Following Statement is MISSPELLED?:",
+        question: "",
+        options: [],
+        answer: ""
+    },
+    {
+        header: "Everything is FALSE in the Following Statement EXCEPT:",
+        question: "",
+        options: [],
+        answer: ""
+    },
+  
+    {
+        header: "Select the CORRECT Answer:",
+        question: "",
+        options: [],
+        answer: ""
+    }
+];
