1b9328743e9bd00db0e0dff4cf90e9c9b82ed716
Kyle <kj.mckinley@gmail.com>
2021-03-13 02:50:46 -0600

    addittional console log edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,9 @@
 // Array of objects containing questions
 var questionAray = [
     {
+        // Instructions displayed above the question for better context on how to answer
         header: "Everything is TRUE in The Following Statement EXCEPT:",
+        // Question that is displayed to the user
         question: "In Return of the Jedi, the rebels attack and destroy the revised Death Star’s shield generator on the planet Endor with the help of furry creatures called Ewoks.",
        // Array of options for the user to select from 
         options: ["The film was actually called 'The Empire Strikes Back'", 
@@ -49,7 +51,8 @@ var questionAray = [
 
 
 // TESTS for the console in the dev tools
-console.log("QUESTION:\n" + questionAray[0].question + "\n------------------------------------------------");
+//OPEN THE CONSOLE TO SEE THE RESULTS!
+console.log("QUESTION:\n---------\n" + questionAray[0].header + "\n-----------------------------------------------------\n" + questionAray[0].question + "\n-----------------------------------------------------");
 
 for(var i = 0; i < questionAray[0].options.length; i++) {
     console.log((i+1) + ". " + questionAray[0].options[i] + "\n--------------------------------------");
